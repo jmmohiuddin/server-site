6d0558e442fbf9ba3e5065fbd6fd1b9ba1441573
jm mohiuddin <jmmohiuddin5@gmail.com>
2021-04-18 15:56:07 +0600

    fc

--- index.js
+++ index.js
@@ -11,26 +11,22 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const MongoClient = require("mongodb").MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q0wlb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kyhh2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 console.log(uri);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 client.connect((err) => {
-  const ServiceCollection = client
-    .db("Programing-Hero-Team")
-    .collection("service-add");
-  const OrderCollection = client.db("Programing-Hero-Team").collection("order");
-  const AdminCollection = client.db("Programing-Hero-Team").collection("admin");
-  const ReviewCollection = client
-    .db("Programing-Hero-Team")
-    .collection("review");
+  const ServiceCollection = client.db("service").collection("service-add");
+  const OrderCollection = client.db("service").collection("order");
+  const AdminCollection = client.db("service").collection("admin");
+  const ReviewCollection = client.db("service").collection("review");
 
   app.post("/addService", (req, res) => {
     const newService = req.body;
@@ -40,6 +36,7 @@ client.connect((err) => {
       res.send(result.insertedCount > 0);
     });
   });
+
   app.get("/services", (req, res) => {
     ServiceCollection.find().toArray((err, items) => {
       res.send(items);
@@ -58,21 +55,12 @@ client.connect((err) => {
   app.post("/isAdmin", (req, res) => {
     const email = req.body.email;
     AdminCollection.find({ email: email }).toArray((err, admin) => {
-      res.send(admin.length > 0);
+      res.send(admin);
     });
   });
-  // app.get('/services', (req, res) => {
-  //     ServiceCollection.find()
-  //     .toArray((err, items) => {
-  //         res.send(items)
-  //     })
-  // })
-
   app.post("/addOrder", (req, res) => {
     const newOrder = req.body;
-    console.log("adding new product: ", newOrder);
     OrderCollection.insertOne(newOrder).then((result) => {
-      console.log("inserted count", result.insertedCount);
       res.send(result.insertedCount > 0);
     });
   });
@@ -86,7 +74,6 @@ client.connect((err) => {
     );
   });
   app.get("/allOrderDetails", (req, res) => {
-    // console.log(req.query.email)
     OrderCollection.find().toArray((err, documents) => {
       res.send(documents);
     });
@@ -121,14 +108,13 @@ client.connect((err) => {
       res.send(items);
     });
   });
-
   app.delete("/delete/:id", (req, res) => {
     const id = ObjectID(req.params.id);
-    console.log("deleted id", id);
-    ServiceCollection.findOneAndDelete({ _id: id }).then((result) => {
-      result.deletedCount > 0;
-    });
+    ServiceCollection.findOneAndDelete({ _id: id }).then((documents) =>
+      res.send(!!documents.value)
+    );
   });
 });
-
-app.listen(process.env.PORT || port);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
